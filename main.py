1097b373befa16d1bca50ff9a42f2b654bfa736e
jpcfarias <j.p.fariasolhoverde@gmail.com>
2022-08-03 18:47:27 -0300

    arquivo contendo os jogos a serem analisados e rascunho dos graficos

--- main.py
+++ main.py
@@ -1,3 +1,28 @@
-#salve rapaziada
-print('Bom dia little john!!!!!')
+from dash import Dash, html, dcc
+import plotly.express as px
+import pandas as pd
 
+app = Dash(__name__)
+
+
+# assume you have a "long-form" data frame
+# see https://plotly.com/python/px-arguments/ for more options
+df = pd.read_excel('jogosfree.xlsx')
+
+fig = px.bar(df, x="name", y="freetoplay", barmode="group")
+
+app.layout = html.Div(children=[
+    html.H1(children='Hello Dash'),
+
+    html.Div(children='''
+        Dash: A web application framework for your data.
+    '''),
+
+    dcc.Graph(
+        id='example-graph',
+        figure=fig
+    )
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
